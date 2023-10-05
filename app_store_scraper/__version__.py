63ec9d462302f3835a3383105f06e335f09274c5
iamfoysal <foysalf652@gmail.com>
2023-10-05 11:27:41 +0600

    initial setup

---
+++ app_store_scraper/__version__.py
@@ -0,0 +1,15 @@
+__title__ = "python-app-store-scraper"
+__version__ = "1.0.0"
+__description__ = "Single API ☝ App Store Review Scraper 🧹"
+__author__ = "Mohammad Foysal"
+__url__ = "https://github.com/iamfoysal/py-app-store"
+__license__ = "MIT"
+__keywords__ = [
+    "app store",
+    "ios",
+    "ios apps",
+    "podcasts",
+    "review",
+    "scraping",
+    "scraper",
+]
